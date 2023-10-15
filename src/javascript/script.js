9b21c97af61fea5878bed447d59cbdac60b49d47
John (Jem) Earl Milam IV <mej7171@gmail.com>
2023-10-15 16:54:33 -0400

    json fix

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,15 +1,37 @@
 var counterContainer = document.querySelector(".website-counter");
-var visitCount = $.ajax({
 
-  url: 'https://resumesite-pyapi.azurewebsites.net/api/HttpVisitor?&name=Azure',
-  type: 'GET',
-  crossDomain: true,
-  dataType: "json",
-    data: {
+// Define the URL of your Azure Function
+const functionUrl = "https://resumesite-pyapi.azurewebsites.net/api/HttpVisitor?&name=Azure";
+
+// Define the data you want to send (if needed)
+const data = {
+    name: "Jem"
+};
+
+// Create an AJAX request using the fetch API
+fetch(functionUrl, {
+    method: "GET", // or "POST" if you need to send data
+    headers: {
+        "Content-Type": "application/json",
     },
-  success: function() { alert(this.data); },
-  error: function() { alert('Failed!'); },
-});
+    // If you need to send data, you can use the following line
+    // body: JSON.stringify(data),
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.text(); // assuming you expect plain text as the response
+    })
+    .then(data => {
+        console.log(data); // "42" should be printed in the console
+        visitCount = data;
+    })
+    .catch(error => {
+        console.error("There was a problem with the fetch operation:", error);
+        visitCount = "0"
+    });
+
 
 //var visitCount = $.get('https://resumesite-pyapi.azurewebsites.net/api/HttpVisitor?&name=Azure');
 
