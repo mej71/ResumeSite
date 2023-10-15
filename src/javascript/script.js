8e69ad690509976438b50bdf6abfc992e4fdaeee
John (Jem) Earl Milam IV <mej7171@gmail.com>
2023-10-15 17:04:17 -0400

    finally

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -22,23 +22,27 @@ fetch(functionUrl, {
         return response.text();
     })
     .then(data => {
-        console.log(data);
+        console.log(data + "A");
         visitCount = data;
+        console.log("why no works?");
     })
     .catch(error => {
         console.error("There was a problem with the fetch operation:", error);
-        visitCount = "0"
-    });
+        visitCount = "0";
+    })
+    .finally( data => {
+      console.log("Test");
+      if (visitCount) {
+        visitCount = Number(visitCount) + 1;
+        //localStorage.setItem("page_view", visitCount);
+      } else {
+        visitCount = 1;
+        //localStorage.setItem("page_view", 1);
+      }
+      counterContainer.innerHTML = visitCount;    
+  });
 
 
 //var visitCount = $.get('https://resumesite-pyapi.azurewebsites.net/api/HttpVisitor?&name=Azure');
 
 // Check if page_view entry is present
-if (visitCount) {
-  visitCount = Number(visitCount) + 1;
-  //localStorage.setItem("page_view", visitCount);
-} else {
-  visitCount = 1;
-  //localStorage.setItem("page_view", 1);
-}
-counterContainer.innerHTML = visitCount;
