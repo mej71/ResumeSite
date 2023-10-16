939f8b4d2d89379fbebcd5327686b0380ca78db2
John (Jem) Earl Milam IV <mej7171@gmail.com>
2023-10-15 23:11:39 -0400

    visit spacing fix

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -22,27 +22,13 @@ fetch(functionUrl, {
         return response.text();
     })
     .then(data => {
-        console.log(data + "A");
         visitCount = data;
-        console.log("why no works?");
     })
     .catch(error => {
         console.error("There was a problem with the fetch operation:", error);
         visitCount = "0";
     })
     .finally( data => {
-      console.log("Test");
-      if (visitCount) {
-        visitCount = Number(visitCount) + 1;
-        //localStorage.setItem("page_view", visitCount);
-      } else {
-        visitCount = 1;
-        //localStorage.setItem("page_view", 1);
-      }
-      counterContainer.innerHTML = visitCount;    
+      visitCount = Number(visitCount) + 1;
+      counterContainer.innerHTML = "Website visit count: " + visitCount;    
   });
-
-
-//var visitCount = $.get('https://resumesite-pyapi.azurewebsites.net/api/HttpVisitor?&name=Azure');
-
-// Check if page_view entry is present
